6e23d3f4162e5d68904dd8e00b6c65f8c60b4c01
“wenbaoxin <1340789826@qq.com>
2021-04-21 16:06:15 +0800

    day(1)

---
+++ 12月份衣服销售数据.py
@@ -0,0 +1,256 @@
+#一号销售数据
+date1 = "1号"
+name1 = "羽绒服"
+price1 = 253.6
+num1 = 500
+xiaoliang1 = 10
+
+#二号销售数据
+date2 = "2号"
+name2 = "牛仔裤"
+price2 = 86.3
+num2 = 600
+xiaoliang2 = 60
+
+#三号销售数据
+date3 = "3号"
+name3 = "风衣"
+price3 = 96.8
+num3 = 335
+xiaoliang3 = 43
+
+#四号销售数据
+date4 = "4号"
+name4 = "皮草"
+price4 = 135.9
+num4 = 855
+xiaoliang4 = 63
+
+#五号销售数据
+date5 = "5号"
+name5 = "T血"
+price5 = 65.8
+num5 = 632
+xiaoliang5 = 63
+
+#六号销售数据
+date6 = "6号"
+name6 = "衬衫"
+price6 = 49.3
+num6 = 562
+xiaoliang6 = 120
+
+#七号销售数据
+date7 = "7号"
+name7 = "牛仔裤"
+price7 = 86.3
+num7 = 600
+xiaoliang7 = 72
+
+#八号销售数据
+date8 = "8号"
+name8 = "羽绒服"
+price8 = 253.6
+num8 = 500
+xiaoliang8 = 69
+
+#九号销售数据
+date9 = "9号"
+name9 = "牛仔裤"
+price9 = 86.3
+num9 = 600
+xiaoliang9 = 35
+
+#十号销售数据
+date10 = "10号"
+name10 = "羽绒服"
+price10 = 253.6
+num10 = 500
+xiaoliang10 = 140
+
+#十一号销售数据
+date11 = "11号"
+name11 = "牛仔裤"
+price11 = 86.3
+num11 = 600
+xiaoliang11 = 90
+
+#十二号销售数据
+date12 = "12号"
+name12 = "皮草"
+price12 = 135.9
+num12 = 855
+xiaoliang12 = 24
+
+#十三号销售数据
+date13 = "13号"
+name13 = "T血"
+price13 = 65.8
+num13 = 632
+xiaoliang13 = 45
+
+#十四号销售数据
+date14 = "14号"
+name14 = "风衣"
+price14 = 96.8
+num14 = 335
+xiaoliang14 = 25
+
+#十五号销售数据
+date15 = "15号"
+name15 = "牛仔裤"
+price15 = 86.3
+num15 = 600
+xiaoliang15 = 60
+
+#十六号销售数据
+date16 = "16号"
+name16 = "T血"
+price16 = 65.8
+num16 = 632
+xiaoliang16 = 129
+
+#十七号销售数据
+date17 = "17号"
+name17 = "羽绒服"
+price17 = 253.6
+num17 = 500
+xiaoliang17 = 10
+
+#十八号销售数据
+date18 = "18号"
+name18 = "风衣"
+price18 = 96.8
+num18 = 335
+xiaoliang18 = 43
+
+#十九号销售数据
+date19 = "19号"
+name19 = "T血"
+price19 = 65.8
+num19 = 632
+xiaoliang19 = 63
+
+#二十号销售数据
+date20 = "20号"
+name20 = "牛仔裤"
+price20 = 86.3
+num20 = 600
+xiaoliang20 = 60
+
+#二十一号销售数据
+date21 = "21号"
+name21 = "皮草"
+price21 = 135.9
+num21 = 855
+xiaoliang21 = 63
+
+#二十二号销售数据
+date22 = "22号"
+name22 = "风衣"
+price22 = 96.8
+num22 = 335
+xiaoliang22 = 60
+
+#二十三号销售数据
+date23 = "23号"
+name23 = "T血"
+price23 = 65.8
+num23 = 632
+xiaoliang23 = 58
+
+#二十四号销售数据
+date24 = "24号"
+name24 = "牛仔裤"
+price24 = 86.3
+num24 = 600
+xiaoliang24 = 140
+
+#二十五号销售数据
+date25 = "25号"
+name25 = "T血"
+price25 = 65.8
+num25 = 632
+xiaoliang25 = 48
+
+#二十六号销售数据
+date26 = "26号"
+name26 = "风衣"
+price26 = 96.8
+num26 = 335
+xiaoliang26 = 43
+
+#二十七号销售数据
+date27 = "27号"
+name27 = "皮草"
+price27 = 135.9
+num27 = 855
+xiaoliang27 = 57
+
+#二十八号销售数据
+date28 = "28号"
+name28 = "羽绒服"
+price28 = 253.6
+num28 = 500
+xiaoliang28 = 10
+
+#二十九号销售数据
+date29 = "29号"
+name29 = "T血"
+price29 = 65.8
+num29 = 632
+xiaoliang29 = 63
+
+#三十号销售数据
+date30 = "30号"
+name30 = "风衣"
+price30 = 96.8
+num30 = 335
+xiaoliang30 = 78
+print("------------------------------十二月衣服销售数据------------------------------")
+print("日期\t\t\t服装名称\t\t\t价格/件\t\t\t库存数量\t\t\t销售量/每日")
+print(date1,"\t\t\t",name1,"\t\t\t",price1,"\t\t\t",num1,"\t\t\t",xiaoliang1)
+print(date2,"\t\t\t",name2,"\t\t\t",price2,"\t\t\t",num2,"\t\t\t",xiaoliang2)
+print(date3,"\t\t\t",name3,"\t\t\t",price3,"\t\t\t",num3,"\t\t\t",xiaoliang3)
+print(date4,"\t\t\t",name4,"\t\t\t",price4,"\t\t\t",num4,"\t\t\t",xiaoliang4)
+print(date5,"\t\t\t",name5,"\t\t\t",price5,"\t\t\t",num5,"\t\t\t",xiaoliang5)
+print(date7,"\t\t\t",name7,"\t\t\t",price7,"\t\t\t",num7,"\t\t\t",xiaoliang7)
+print(date8,"\t\t\t",name8,"\t\t\t",price8,"\t\t\t",num8,"\t\t\t",xiaoliang8)
+print(date9,"\t\t\t",name9,"\t\t\t",price9,"\t\t\t",num9,"\t\t\t",xiaoliang9)
+print(date10,"\t\t\t",name10,"\t\t\t",price10,"\t\t\t",num10,"\t\t\t",xiaoliang10)
+print(date11,"\t\t\t",name11,"\t\t\t",price11,"\t\t\t",num11,"\t\t\t",xiaoliang11)
+print(date12,"\t\t\t",name12,"\t\t\t",price12,"\t\t\t",num12,"\t\t\t",xiaoliang12)
+print(date13,"\t\t\t",name13,"\t\t\t",price13,"\t\t\t",num13,"\t\t\t",xiaoliang13)
+print(date14,"\t\t\t",name14,"\t\t\t",price14,"\t\t\t",num14,"\t\t\t",xiaoliang14)
+print(date15,"\t\t\t",name15,"\t\t\t",price15,"\t\t\t",num15,"\t\t\t",xiaoliang15)
+print(date16,"\t\t\t",name16,"\t\t\t",price16,"\t\t\t",num16,"\t\t\t",xiaoliang16)
+print(date17,"\t\t\t",name17,"\t\t\t",price17,"\t\t\t",num17,"\t\t\t",xiaoliang17)
+print(date18,"\t\t\t",name18,"\t\t\t",price18,"\t\t\t",num18,"\t\t\t",xiaoliang18)
+print(date19,"\t\t\t",name19,"\t\t\t",price19,"\t\t\t",num19,"\t\t\t",xiaoliang19)
+print(date20,"\t\t\t",name20,"\t\t\t",price20,"\t\t\t",num20,"\t\t\t",xiaoliang20)
+print(date21,"\t\t\t",name21,"\t\t\t",price21,"\t\t\t",num21,"\t\t\t",xiaoliang21)
+print(date22,"\t\t\t",name22,"\t\t\t",price22,"\t\t\t",num22,"\t\t\t",xiaoliang22)
+print(date23,"\t\t\t",name23,"\t\t\t",price23,"\t\t\t",num23,"\t\t\t",xiaoliang23)
+print(date24,"\t\t\t",name24,"\t\t\t",price24,"\t\t\t",num24,"\t\t\t",xiaoliang24)
+print(date25,"\t\t\t",name25,"\t\t\t",price25,"\t\t\t",num25,"\t\t\t",xiaoliang25)
+print(date26,"\t\t\t",name26,"\t\t\t",price26,"\t\t\t",num26,"\t\t\t",xiaoliang26)
+print(date27,"\t\t\t",name27,"\t\t\t",price27,"\t\t\t",num27,"\t\t\t",xiaoliang27)
+print(date28,"\t\t\t",name28,"\t\t\t",price28,"\t\t\t",num28,"\t\t\t",xiaoliang28)
+print(date29,"\t\t\t",name29,"\t\t\t",price29,"\t\t\t",num29,"\t\t\t",xiaoliang29)
+print(date30,"\t\t\t",name30,"\t\t\t",price30,"\t\t\t",num30,"\t\t\t",xiaoliang30)
+
+moneyall=(xiaoliang1+xiaoliang8+xiaoliang10+xiaoliang17+xiaoliang28)*253.6+(xiaoliang2+xiaoliang7+xiaoliang9+xiaoliang11+xiaoliang15+xiaoliang20+xiaoliang24)*86.3+(xiaoliang3+xiaoliang14+xiaoliang18+xiaoliang22+xiaoliang26+xiaoliang30)*96.8+(xiaoliang4+xiaoliang12+xiaoliang21+xiaoliang27)*135.9+(xiaoliang5+xiaoliang13+xiaoliang16+xiaoliang19+xiaoliang23+xiaoliang25+xiaoliang29)*65.8+xiaoliang6*49.3
+xiaoliangall = xiaoliang1+xiaoliang2+xiaoliang3+xiaoliang4+xiaoliang5+xiaoliang6+xiaoliang7+xiaoliang8+xiaoliang9+xiaoliang10+xiaoliang11+xiaoliang12+xiaoliang13+xiaoliang14+xiaoliang15+xiaoliang16+xiaoliang17+xiaoliang18+xiaoliang19+xiaoliang20+xiaoliang21+xiaoliang22+xiaoliang23+xiaoliang24+xiaoliang25+xiaoliang26+xiaoliang27+xiaoliang28+xiaoliang29+xiaoliang30
+print("本月总销售额为：",round(moneyall,4))
+print("羽绒服的销售占比：",round((xiaoliang1+xiaoliang8+xiaoliang10+xiaoliang17+xiaoliang28)/xiaoliangall,2))
+print("牛仔裤的销售占比：",round((xiaoliang2+xiaoliang7+xiaoliang9+xiaoliang11+xiaoliang15+xiaoliang20+xiaoliang24)/xiaoliangall,2))
+print("风衣的销售占比：",round((xiaoliang3+xiaoliang14+xiaoliang18+xiaoliang22+xiaoliang26+xiaoliang30)/xiaoliangall,2))
+print("皮草的销售占比：",round((xiaoliang4+xiaoliang12+xiaoliang21+xiaoliang27)/xiaoliangall,2))
+print("T血的销售占比：",round((xiaoliang5+xiaoliang13+xiaoliang16+xiaoliang19+xiaoliang23+xiaoliang25+xiaoliang29)/xiaoliangall,2))
+print("衬衫的销售占比：",round((xiaoliang6)/xiaoliangall,2))
+print("羽绒服的销售额占比：",round((xiaoliang1+xiaoliang8+xiaoliang10+xiaoliang17+xiaoliang28)*253.6/moneyall,2))
+print("牛仔裤的销售额占比：",round((xiaoliang2+xiaoliang7+xiaoliang9+xiaoliang11+xiaoliang15+xiaoliang20+xiaoliang24)*86.3/moneyall,2))
+print("风衣的销售额占比：",round((xiaoliang3+xiaoliang14+xiaoliang18+xiaoliang22+xiaoliang26+xiaoliang30)*96.8/moneyall,2))
+print("皮草的销售额占比：",round((xiaoliang4+xiaoliang12+xiaoliang21+xiaoliang27)*135.9/moneyall,2))
+print("T血的销售额占比：",round((xiaoliang5+xiaoliang13+xiaoliang16+xiaoliang19+xiaoliang23+xiaoliang25+xiaoliang29)*65.8/moneyall,2))
+print("衬衫的销售额占比：",round(xiaoliang6*49.3/moneyall,2))
